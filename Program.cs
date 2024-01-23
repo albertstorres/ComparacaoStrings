2e99fb21e696c0d2abddd41f0ec1b631bed8fa10
Albert Sevy <albertstorres@gmail.com>
2024-01-23 20:17:48 -0300

    Finalizado o repositório da aula Strings/Comparação de Strings

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ComparacaoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "testando";
+            Console.WriteLine(texto.CompareTo("1"));//CompareTo() COMPARA STRINGS IDÊNTICAS. RETORNA 0=SIM, 1=NÃO E -1=QUANDO LETRA MAIÚSCULA
+            Console.WriteLine(texto.Contains("ando"));//Contains() COMPARA SE AO MENOS UM CARACTERE FAZ PARTE DA STRING. RETORNA BOOLEAN
+            Console.WriteLine(texto.Contains("TESt", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(texto.CompareTo("Testando"));//RETORNA -1
+        }
+    }
+}
